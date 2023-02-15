5e9eba6352787ab425097f18ebd23f4b4e1e3cf6
Chen062688 <111212895+Chen062688@users.noreply.github.com>
2023-02-15 10:24:29 +0800

    pull commit

--- src/main/java/chen/cc/gitmanage/Config/git.java
+++ src/main/java/chen/cc/gitmanage/Config/git.java
@@ -14,5 +14,6 @@ public class git {
         System.out.println("hello git4");
         System.out.println("master-test");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
