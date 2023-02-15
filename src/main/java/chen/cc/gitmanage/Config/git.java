b738531dfb05f4eb6a0ab116ba7d451c5f176863
Chen <1617718746@qq.com>
2023-02-15 09:03:21 +0800

    hot-fix commit1

--- src/main/java/chen/cc/gitmanage/Config/git.java
+++ src/main/java/chen/cc/gitmanage/Config/git.java
@@ -1,9 +1,16 @@
 package chen.cc.gitmanage.Config;
 
+/**
+ * git
+ *
+ * @author 张立晨
+ * @date 2023/02/15
+ */
 public class git {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
