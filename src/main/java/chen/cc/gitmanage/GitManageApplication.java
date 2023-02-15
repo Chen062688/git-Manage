1ee985e80d2eaadfe12ac9dbfac356c3b69e6635
Chen <1617718746@qq.com>
2023-02-15 08:47:51 +0800

    first commit

---
+++ src/main/java/chen/cc/gitmanage/GitManageApplication.java
@@ -0,0 +1,13 @@
+package chen.cc.gitmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chen/cc/gitmanage/GitManageApplicationTests.java
@@ -0,0 +1,13 @@
+package chen.cc.gitmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
